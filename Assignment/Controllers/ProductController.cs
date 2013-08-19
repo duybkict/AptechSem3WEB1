3f15cf299f5abe1304fefb69ca6154713cf12a23
Duy Trần <duytran.bkict@gmailc.om>
2013-08-19 11:57:37 +0700

    Add product details page

---
+++ Assignment/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using MvcAssignment.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAssignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index(int id = 0)
+        {
+			// Initialize datacontext
+			ElectonicShopDataContext db = new ElectonicShopDataContext();
+
+			Product product = (from p in db.Products
+							   where p.id == id
+							   select p).FirstOrDefault();
+
+			if (product == null) {
+				return RedirectToAction("Index", "Products", null);
+			}
+
+			ViewData["product"] = product;
+
+            return View();
+        }
+
+    }
+}
