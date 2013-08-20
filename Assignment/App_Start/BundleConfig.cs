fd7677e647feb1b4f176e373cd6efb6715c11ccf
Duy Trần <duytran.bkict@gmailc.om>
2013-08-20 19:44:12 +0700

    Update to publish

--- Assignment/App_Start/BundleConfig.cs
+++ Assignment/App_Start/BundleConfig.cs
@@ -17,11 +17,9 @@ namespace MvcAssignment
 
 			bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
 						"~/Content/bootstrap.css",
-						"~/Content/bootstrap-responsive.css"));
-
-			bundles.Add(new StyleBundle("~/Content/fonts").Include(
-						"~/Content/MyriadPro.css",
-						"~/Content/OpenSans.css"));
+						"~/Content/bootstrap-responsive.css",
+						"~/Content/OpenSans.css",
+						"~/Content/MyriadPro.css"));
 
 			bundles.Add(new StyleBundle("~/Content/custom").Include(
 						"~/Content/style.css"));
